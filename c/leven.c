6473b59d51ed5bcd10dc9e7b5a7d3af74259b0fd
Scott Sherrill-Mix <sherrillmix@users.noreply.github.com>
2011-09-16 17:01:33 +0000

    Finished up alignments
    git-svn-id: https://microb98.med.upenn.edu/svn/scott/scripts/R@3452 acd2042c-187b-11de-8279-d8d34a11d372

--- c/leven.c
+++ c/leven.c
@@ -31,23 +31,43 @@ void bestMismatch(int *ans, char **s1, char **s2, int *weights){
 	}
 }
 
-void levenAll(int *answer, char **s1, char **s2, int *homoLimit, int *prepend, int *append, int *debug) {
+void revString(char *string, int nChar){
+	if(nChar<2)return;
+	int i;
+	char tmp;
+	for(i=0;i<nChar/2;i++){ //using integer division to round down
+		tmp=string[i];
+		string[i]=string[nChar-1-i];
+		string[nChar-1-i]=tmp;
+	}
+}
+
+void levenAll(int *answer, char **s1, char **s2, int *homoLimit, int *prepend, int *append, int *debug, int *isAlign, char **align) {
+	unsigned int i,j;//counters
 	unsigned int cost_del = 1;
 	unsigned int cost_ins = 1;
 	const unsigned int cost_sub = 1;
-	unsigned int n1 = strlen(s1[0]);// s1.length();
+	unsigned int n1 = strlen(s1[0]);
 	unsigned int n2 = strlen(s2[0]);
 	//printf("%d %d\n",n1,n2);
 	unsigned int* endCol = malloc(sizeof(unsigned int)*(n1+1));
 	unsigned int* lastRow = malloc(sizeof(unsigned int)*(n2+1));
-	unsigned int* thisRow = malloc(sizeof(unsigned int)*(n2+1));//alloc unsigned int[n2+1];
-	unsigned int* cost_dels = malloc(sizeof(unsigned int)*(n1));
-	unsigned int* cost_inss = malloc(sizeof(unsigned int)*(n2));
+	unsigned int* thisRow = malloc(sizeof(unsigned int)*(n2+1));
+	unsigned int* cost_dels = malloc(sizeof(unsigned int)*(n1)); //store costs derived from homopolymers
+	unsigned int* cost_inss = malloc(sizeof(unsigned int)*(n2)); //store costs derived from homopolymers
 	unsigned int* tmpRow;
 	unsigned int min;
+	unsigned int **array;
+	unsigned int appendTracker; //keep track of location for alignment when we're doing ends free
+	unsigned int coords[3],newCoords[2],upCoord,leftCoord; //for tracing back
+	if(isAlign[0]){
+		//Make giant array
+		array=malloc(sizeof(unsigned int *)*(n1+1));
+		for(i = 0;i<n1+1;i++)array[i]=malloc(sizeof(unsigned int)*(n2+1));
+	}
 
 	if(*homoLimit){
-		for(unsigned int i = 0; i < n1; i++){
+		for(i = 0; i < n1; i++){
 			cost_del=0;
 			if(i>=*homoLimit){
 				for(unsigned int step = 1; step <= *homoLimit; step++){
@@ -60,7 +80,7 @@ void levenAll(int *answer, char **s1, char **s2, int *homoLimit, int *prepend, i
 			}else cost_del=1;
 			cost_dels[i]=cost_del;
 		}
-		for(unsigned int j = 0; j < n2; j++){
+		for(j = 0; j < n2; j++){
 			cost_ins=0;
 			if(j>=*homoLimit){
 				for(unsigned int step = 1; step <= *homoLimit; step++){
@@ -73,24 +93,27 @@ void levenAll(int *answer, char **s1, char **s2, int *homoLimit, int *prepend, i
 			cost_inss[j]=cost_ins;
 		}
 	}else{
-		for(unsigned int i = 0; i < n1; i++) cost_dels[i]=1;
-		for(unsigned int j = 0; j < n2; j++) cost_inss[j]=1;
+		for(i = 0; i < n1; i++) cost_dels[i]=1;
+		for(j = 0; j < n2; j++) cost_inss[j]=1;
 	}
 
 	lastRow[0] = 0;
 	//set first row to 0s if append ok on first string
-	if(prepend[1]) for(unsigned int j = 1; j <= n2; j++) lastRow[j] = 0;
-	else for(unsigned int j = 1; j <= n2; j++) lastRow[j] = lastRow[j-1] + cost_inss[j-1];
+	if(prepend[1]) for(j = 1; j <= n2; j++) lastRow[j] = 0;
+	else for(j = 1; j <= n2; j++) lastRow[j] = lastRow[j-1] + cost_inss[j-1];
+	if(isAlign[0]){
+		for(j=0;j<=n2;j++)array[0][j]=lastRow[j];
+	}
 	
 	endCol[0]=lastRow[n2];
 
 	//for (int i = 1; i <=n2; ++i) printf(" %d ",q[i]);
-	for(unsigned int i = 1; i <= n1; i++){
+	for(i = 1; i <= n1; i++){
 		cost_del=cost_dels[i-1];
 		//Set first column to 0s if prepend ok on first string
 		if(prepend[0]) thisRow[0] = lastRow[0];
 		else thisRow[0] = lastRow[0] + cost_del;
-		for(unsigned int j = 1; j <= n2; j++){
+		for(j = 1; j <= n2; j++){
 			//printf("S1[%d]:%c S2[%d]:%c\n",i-1,s1[0][i-1],j-1,s2[0][j-1]);
 			cost_ins=cost_inss[j-1];
 			unsigned int d_del = lastRow[j] + cost_del;
@@ -103,6 +126,10 @@ void levenAll(int *answer, char **s1, char **s2, int *homoLimit, int *prepend, i
 		}
 		//keep track of the final column for substringing
 		endCol[i]=thisRow[n2];
+		//store this row if we're aligning (could store directly instead)
+		if(isAlign[0]){
+			for(j=0;j<=n2;j++)array[i][j]=thisRow[j];
+		}
 		//Switch the pointers around
 		tmpRow = lastRow;
 		lastRow = thisRow;
@@ -113,19 +140,99 @@ void levenAll(int *answer, char **s1, char **s2, int *homoLimit, int *prepend, i
 	*answer = lastRow[n2];
 	if(append[1]){
 		min=*answer;
-		for( unsigned int j = 0; j <= n2; ++j ){
-			if(lastRow[j]<min)min=lastRow[j];
+		appendTracker=n2;
+		for(j = 0; j <= n2; ++j ){
+			if(lastRow[j]<min){
+				min=lastRow[j];
+				appendTracker=j;
+			}
 		}
 		*answer = min;
 	}
 	if(append[0]){
 		min=*answer;
+		appendTracker=n2;
 		if(*debug)printf("Last column: ");
-		for( unsigned int i = 0; i <= n1; ++i ){
+		for(i = 0; i <= n1; ++i ){
 			if(*debug)printf(" %d ",endCol[i]);
-			if(endCol[i]<min)min=endCol[i];
+			if(endCol[i]<min){
+				min=endCol[i];
+				appendTracker=i;
+			}
 		}
 		*answer = min;
+	}
+	if(isAlign[0]){
+		coords[0]=n1;
+		coords[1]=n2;
+		coords[2]=0;
+		if(append[0]){
+			for(i=n1;i>appendTracker;i--){
+				align[0][coords[2]]=s1[0][coords[0]];
+				align[1][coords[2]]='-';	
+				coords[0]--;
+				coords[2]++;
+				if(i==0)exit(1);//if this happens we're going to wrap around (shouldn't happen)
+			}
+		}
+		if(append[1]){
+			for(j=n2;j>appendTracker;j--){
+				align[0][coords[2]]='-';	
+				align[1][coords[2]]=s2[0][coords[1]];
+				coords[1]--;
+				coords[2]++;
+				if(j==0)exit(1);//if this happens we're going to wrap around (shouldn't happen)
+			}
+		}
+		//could reprogram to look for least gappy alignment?
+		while(coords[0]!=0||coords[1]!=0){
+			//deal with edge of array
+			upCoord=coords[0]<1?0:coords[0]-1;
+			leftCoord=coords[1]<1?0:coords[1]-1;
+			//find min of three choices
+			newCoords[0]=upCoord;newCoords[1]=leftCoord;
+			min=array[upCoord][leftCoord];
+			if(array[upCoord][coords[1]]<min){
+				min=array[upCoord][coords[1]];
+				newCoords[0]=upCoord;
+				newCoords[1]=coords[1];
+			}
+			if(array[coords[0]][leftCoord]<min){
+				min=array[coords[0]][leftCoord];
+				newCoords[0]=coords[0];
+				newCoords[1]=leftCoord;
+			}
+			if(newCoords[0]==coords[0]){
+				align[0][coords[2]]='-';	
+			}else{
+				align[0][coords[2]]=s1[0][coords[0]-1];
+				coords[0]--;
+			}
+			if(newCoords[1]==coords[1]){
+				align[1][coords[2]]='-';	
+			}else{
+				align[1][coords[2]]=s2[0][coords[1]-1];
+				coords[1]--;
+			}
+			//printf("New %d %d left %d up %d coords[2] %d\n",coords[0],coords[1],leftCoord,upCoord,coords[2]);
+			coords[2]++;
+		}
+		align[0][coords[2]]='\0';
+		align[1][coords[2]]='\0';
+		//reverse these strings
+		revString(align[0],coords[2]);
+		revString(align[1],coords[2]);
+		if(*debug)printf("%s\n%s\n",align[0],align[1]);
+
+		if(*debug){
+			for(i=0;i<n1+1;i++){
+				for(j=0;j<n2+1;j++){
+				printf("%d ",array[i][j]);
+				}
+				printf("\n");
+			}
+		}
+			
 	}
 	//printf(" %d ",*answer);
 	free(endCol);
@@ -133,7 +240,10 @@ void levenAll(int *answer, char **s1, char **s2, int *homoLimit, int *prepend, i
 	free(thisRow);
 	free(cost_dels);
 	free(cost_inss);
-	
+	if(isAlign[0]){
+		for(i = 0;i<n1+1;i++)free(array[i]);
+		free(array);
+	}
 }
 
 
@@ -195,7 +305,7 @@ void levenAlign(int *answer, char **align, char **s1, char **s2,int *subString, 
 	traceback[0][0]=0;
 
 	p[0] = 0;
-	for( unsigned int j = 1; j <= n2; ++j ){
+	for(unsigned int j = 1; j <= n2; ++j ){
 		if(*subString) p[j] = 0;
 		else p[j] = p[j-1] + cost_ins;
 		traceback[0][j] = INS_ID;	
@@ -216,7 +326,7 @@ void levenAlign(int *answer, char **align, char **s1, char **s2,int *subString, 
 		}
 		q[0] = p[0] + cost_del;
 		traceback[i][0] = DEL_ID;	
-		for( unsigned int j = 1; j <= n2; ++j ){
+		for(unsigned int j = 1; j <= n2; ++j ){
 			cost_ins=1;
 			if(*homoLimit&&j>*homoLimit){
 				cost_ins=0;
@@ -264,7 +374,7 @@ void levenAlign(int *answer, char **align, char **s1, char **s2,int *subString, 
 	int cursory=n2;
 	if(*subString){
 		min=p[0];
-		for( unsigned int j = 1; j <= n2; ++j ){
+		for(unsigned int j = 1; j <= n2; ++j ){
 			if(p[j]<min){
 				min=p[j];
 				cursory=j-1;
--- levenshtein.R
+++ levenshtein.R
@@ -43,7 +43,42 @@ bestMismatch <- function(pattern, subject, pos=findAll, weights=rep(1,nchar(patt
 	}
 }
 
-levenAll <- function(string1, string2,distance=FALSE,homoLimit=0,debug=FALSE,prepend=NULL,append=NULL) {
+
+alignStringsToString<-function(strings,ref,homoLimit=0,prepend=NULL,append=NULL,substring1=FALSE,substring2=FALSE){
+	if(substring1){append<-c(append,1);prepend<-c(prepend,1)}
+	if(substring2){append<-c(append,2);prepend<-c(prepend,2)}
+	append<-1:2 %in% append
+	prepend<-1:2 %in% prepend
+	aligns<-lapply(strings,function(x)levenAll(x,ref,homoLimit=homoLimit,prepend=prepend,append=append,align=TRUE)[[2]])
+	refs<-c(ref,sapply(aligns,'[[',2))
+	aligns<-c(ref,sapply(aligns,'[[',1))
+	print(refs)
+	print(aligns)
+	out<-rep('',length(strings)+1)
+	#rather inefficient
+	while(any(nchar(refs)>0)){
+		selector<-substring(refs,1,1)=='-'
+		if(any(selector)){
+			out[!selector]<-sprintf('%s%s',out[!selector],'-')
+			refs[selector]<-substring(refs[selector],2)
+			out[selector]<-sprintf('%s%s',out[selector],substring(aligns[selector],1,1))
+			aligns[selector]<-substring(aligns[selector],2)
+		}else{
+			out<-sprintf('%s%s',out,substring(aligns,1,1))
+			refs<-substring(refs,2)
+			aligns<-substring(aligns,2)
+		}
+	}
+	#refGaps<-lapply(gregexpr('-',refs,fixed=TRUE),function(x)x[x!=-1])
+	#uniqueGaps<-sort(unique(unlist(refGaps)))
+	#finalOut<-vector('list',length(strings)+1)
+	#for(i in uniqueGaps){
+		#selector<-sapply(refGaps,function(x)i %in% refGaps)#could optimize here
+	#}
+	return(list('ref'=out[1],'align'=out[-1]))
+}
+
+levenAll <- function(string1, string2,distance=FALSE,homoLimit=0,debug=FALSE,prepend=NULL,append=NULL,align=FALSE) {
 	if(is.null(prepend))prepend<-c(FALSE,FALSE)
 	if(is.null(append))append<-c(FALSE,FALSE)
 	if(length(append)!=2&length(prepend!=2))stop(simpleError('Specify prepend and append as a vector of two logicals'))
@@ -57,15 +92,25 @@ levenAll <- function(string1, string2,distance=FALSE,homoLimit=0,debug=FALSE,pre
 	n1<-nchar(string1)
 	n2<-nchar(string2)
 	if(is.na(string1)|is.na(string2))stop(simpleError('NULL or NA string in levenAll'))
-	####WORK HERE, CHECK APPEND PREPEND IF 1|1 2|2 |1 |2 1| 2| 1,2| |1,2 THEN RUN ONCE 1,2|1,2 1|2 2|1 1,2|1 1,2|2 1|1,2 2|1,2 RUN TWICE
-	ans<-sapply(runs,function(x).C('levenAll',as.integer(1),as.character(string1),as.character(string2),as.integer(homoLimit),as.integer(x[1:2]),as.integer(x[3:4]),as.integer(debug))[[1]][1])
+	
+	#don't forget to deal with runs
+	if(align){
+		isAlign<-1
+		outStrings<-rep(paste(rep('Z',2*(n1+n2)),collapse=''),2)
+	}else{
+		isAlign<-0
+		outStrings<-c('','')
+	}
+	ans<-lapply(runs,function(x).C('levenAll',as.integer(1),as.character(string1),as.character(string2),as.integer(homoLimit),as.integer(x[1:2]),as.integer(x[3:4]),as.integer(debug),as.integer(isAlign),as.character(outStrings))[c(1,9)])
 	#FIX UP DISTANCE CALCULATION
 #	if(distance)ans[[1]]
 #		ans2<-.C('levenAll',as.integer(1),as.character(string2),as.character(string1),as.integer(subString),as.integer(homoLimit),as.integer(prepend),as.integer(append),as.integer(debug))
 #		if(distance){if(ans2[[1]][1]/n2<ans[[1]][1]/n1){ans<-ans2;string1<-string2;n1<-n2}} #replacing answer and string for later calculation
 #		else{if(ans2[[1]][1]<ans[[1]][1])ans<-ans2}
 #	}
-	output<-min(ans)
+	selector<-which.min(sapply(ans,'[[',1))
+	if(!align)output<-ans[[selector]][[1]]
+	else output<-ans[[selector]]
 	#if (distance){
 	#	if(subString) return(ans[[1]][1]/n1)
 	#	else return(ans[[1]][1]/max(n1,n2))
